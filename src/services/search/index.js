b7bbec1147eee35466639b1437b9ef6e9ec25ca3
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2022-04-24 18:03:44 -0300

    fix: nome da rota incorreto

--- src/services/search/index.js
+++ src/services/search/index.js
@@ -2,7 +2,7 @@ import {api} from '../config'
 const route = '/search'
 
 export async function searchSongsAndArtist({term}){
-  return api.get(rota, {
+  return api.get(route, {
     headers:{
       'X-RapidAPI-Host': 'shazam.p.rapidapi.com',
       'X-RapidAPI-Key': '8d0f50387fmsh1525828353ba47fp15d4ebjsn3e2fd8403f6f'
